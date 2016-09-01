651a5b620f0af306897878e6cd81c31e6a938906
David Griffiths <dgriff_67@yahoo.com>
2016-09-01 14:45:15 +0100

    Initial commit for Project 2

---
+++ index.php
@@ -0,0 +1,89 @@
+<?php
+
+$htmlBody = <<<END
+<form method="GET">
+  <div>
+    Search Term: <input type="search" id="q" name="q" placeholder="Enter Search Term">
+  </div>
+  <div>
+    Max Results: <input type="number" id="maxResults" name="maxResults" min="1" max="50" step="1" value="25">
+  </div>
+  <input type="submit" value="Search">
+</form>
+END;
+
+// This code will execute if the user entered a search query in the form
+// and submitted the form. Otherwise, the page displays the form above.
+if (isset($_GET['q']) && isset($_GET['maxResults'])) {
+  // Call set_include_path() as needed to point to your client library.
+//require_once '..\google\google-api-php-client-master\src\Google\Client.php';
+//require_once '..\google\google-api-php-client-master\src\Google\Service.php';
+require_once '.\vendor\autoload.php';
+
+  /*
+   * Set $DEVELOPER_KEY to the "API key" value from the "Access" tab of the
+   * Google Developers Console <https://console.developers.google.com/>
+   * Please ensure that you have enabled the YouTube Data API for your project.
+   */
+  $DEVELOPER_KEY = 'AIzaSyCiFuJxg2zt20dZgRyw5Q7w1sajIORrBEg';
+
+  $client = new Google_Client();
+  $client->setDeveloperKey($DEVELOPER_KEY);
+
+  // Define an object that will be used to make all API requests.
+  $youtube = new Google_Service_YouTube($client);
+
+  try {
+    // Call the search.list method to retrieve results matching the specified
+    // query term.
+    $searchResponse = $youtube->search->listSearch('id,snippet', array(
+      'q' => $_GET['q'],
+      'maxResults' => $_GET['maxResults'],
+    ));
+
+    $videos = '';
+    $channels = '';
+    $playlists = '';
+
+    // Add each result to the appropriate list, and then display the lists of
+    // matching videos, channels, and playlists.
+    foreach ($searchResponse['items'] as $searchResult) {
+      switch ($searchResult['id']['kind']) {
+        case 'youtube#video':  
+          $videos .= sprintf('<li>%s (%s)</li>', $searchResult['snippet']['title'],  
+            $searchResult['id']['videoId']."<a href=http://www.youtube.com/watch?v=".$searchResult['id']['videoId']." target=_blank>   Watch This Video</a>");  
+          break;  
+        case 'youtube#channel':  
+          $channels .= sprintf('<li>%s (%s)</li>', $searchResult['snippet']['title'],  
+            $searchResult['id']['channelId']);  
+          break;  
+      }
+    }
+
+    $htmlBody .= <<<END
+    <h3>Videos</h3>
+    <ul>$videos</ul>
+    <h3>Channels</h3>
+    <ul>$channels</ul>
+    <h3>Playlists</h3>
+    <ul>$playlists</ul>
+END;
+  } catch (Google_Service_Exception $e) {
+    $htmlBody .= sprintf('<p>A service error occurred: <code>%s</code></p>',
+      htmlspecialchars($e->getMessage()));
+  } catch (Google_Exception $e) {
+    $htmlBody .= sprintf('<p>An client error occurred: <code>%s</code></p>',
+      htmlspecialchars($e->getMessage()));
+  }
+}
+?>
+
+<!doctype html>
+<html>
+  <head>
+    <title>YouTube Search</title>
+  </head>
+  <body>
+    <?=$htmlBody?>
+  </body>
+</html>
