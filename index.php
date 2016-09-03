09c62d997549e7b6d2451fd9d3b8b16dfa084831
David Griffiths <dgriff_67@yahoo.com>
2016-09-03 19:13:54 +0100

    Using Bootstrap responsive styling

--- index.php
+++ index.php
@@ -1,14 +1,17 @@
 <?php
-
+//Heredoc
 $htmlBody = <<<END
+<h3>Search YouTube</h3>
 <form method="GET">
-  <div>
-    Search Term: <input type="search" id="q" name="q" placeholder="Enter Search Term">
-  </div>
-  <div>
-    Max Results: <input type="number" id="maxResults" name="maxResults" min="1" max="50" step="1" value="25">
-  </div>
-  <input type="submit" value="Search">
+    <div class="form-group" class="col-xs-4">
+        <label for="q">Search:</label>
+        <input type="text" class="form-control" id="q" name="q" placeholder="Enter Search Term">
+    </div>
+    <div class="form-group">
+        <label for="maxResults">Max Results:</label>
+        <input type="number" id="maxResults" name="maxResults" min="1" max="50" step="1" value="5">
+    </div>
+    <button type="submit" class="btn btn-default">Search</button>
 </form>
 END;
 
@@ -48,27 +51,27 @@ require_once '.\vendor\autoload.php';
     $playlists = '';
 
     // Add each result to the appropriate list, and then display the lists of
-    // matching videos, channels, and playlists.
+    // matching videos. We are not interested in channels and plylists here 
     foreach ($searchResponse['items'] as $searchResult) {
       switch ($searchResult['id']['kind']) {
         case 'youtube#video':  
-          $videos .= sprintf('<li>%s (%s)</li>', $searchResult['snippet']['title'],  
-            $searchResult['id']['videoId']."<a href=http://www.youtube.com/watch?v=".$searchResult['id']['videoId']." target=_blank>   Watch This Video</a>");  
-          break;  
-        case 'youtube#channel':  
-          $channels .= sprintf('<li>%s (%s)</li>', $searchResult['snippet']['title'],  
-            $searchResult['id']['channelId']);  
+          $videos .= sprintf(' <li class="list-group-item"><input type="checkbox" name="favourite[]" id="favourite" value="%s:%s"> %s (%s)</li>', $searchResult['snippet']['title'], $searchResult['id']['videoId'], $searchResult['snippet']['title'],    
+          "<a href=http://www.youtube.com/watch?v=".$searchResult['id']['videoId']." target=_blank> Watch This Video</a>"); 
           break;  
       }
     }
-
+    //More Heredoc
     $htmlBody .= <<<END
     <h3>Videos</h3>
-    <ul>$videos</ul>
-    <h3>Channels</h3>
-    <ul>$channels</ul>
-    <h3>Playlists</h3>
-    <ul>$playlists</ul>
+    <form action="addfavourite.php" method = "POST">
+        <div class="form-group">
+            <ul class="list-group">
+                $videos
+            </ul>
+        </div>
+        <button type="submit" class="btn btn-default">Add Favourite</button>
+    </form>
+
 END;
   } catch (Google_Service_Exception $e) {
     $htmlBody .= sprintf('<p>A service error occurred: <code>%s</code></p>',
@@ -84,8 +87,18 @@ END;
 <html>
   <head>
     <title>YouTube Search</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link href="http://twitter.github.com/bootstrap/assets/css/bootstrap.css" rel="stylesheet">
+  <link href = "css/youtube_favourites.css" rel = "stylesheet">
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  
+    
   </head>
   <body>
-    <?=$htmlBody?>
+    <?php echo $htmlBody?>
   </body>
 </html>
