807a7604f9e474f0b5e5d87a8d15c51e85b3b6d3
David Griffiths <dgriff_67@yahoo.com>
2016-09-07 18:31:48 +0100

    fixed so now deletes favourite_tags first before deleting tags

--- deletefavourite.php
+++ deletefavourite.php
@@ -1,8 +1,7 @@
 <?php
 
-
     if(isset($_POST['favourite'])) {
-    $name = $_POST['favourite'];
+    $favourites = (array)$_POST['favourite'];
         try {
         //connection details for database held in config.ini file
         //parse the ini file to retrieve connection details
@@ -15,16 +14,25 @@
         $db = new PDO("mysql:host=$host;dbname=$dbname",$user,$pass);
         //we want PDO to throw an informative exception if there is a problem
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        //First we need to delete all favourite_tags with this favourite_id
+        $stmt = $db->prepare("DELETE from favourite_tags where favouriteid_FK = :favourite_id"); 
+        foreach ($favourites as $favourite){
+            //echo $favourite;
+            $stmt->execute(array(
+            ":favourite_id" => $favourite)
+            );
+        }
         //we are going to use prepared statements for clean coding and to defend against SQL injection
-        $stmt = $db->prepare("delete from favourites where videoid = :videoid");  
+        
+        $stmt = $db->prepare("delete from favourites where favourite_id = :favourite_id");  
                         
-            foreach ($name as $favourite){
-                $stmt->execute(array(
-                ":videoid" => $favourite)
-                );
-            }
-            $stmt->closeCursor();
-            header("Location: searchfavourites.php");
+        foreach ($favourites as $favourite){
+            $stmt->execute(array(
+            ":favourite_id" => $favourite)
+            );
+        }
+        $stmt->closeCursor();
+        header("Location: searchfavourites.php");
         } 
         catch (PDOException $e) {
             printf("We have a problem: %s\n ", $e->getMessage());
