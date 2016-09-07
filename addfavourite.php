b06d36c8cae1e062fe11b6c4e6d579591c422577
David Griffiths <dgriff_67@yahoo.com>
2016-09-07 18:30:40 +0100

    now adds new tags and checkbox tags and delivers a success message with link

--- addfavourite.php
+++ addfavourite.php
@@ -1,12 +1,20 @@
+<!doctype html>
+<html>
+  <head>
+    <title>Search YouTube Favourites</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link href="http://twitter.github.com/bootstrap/assets/css/bootstrap.css" rel="stylesheet">
+    <link href = "css/youtube_favourites.css" rel = "stylesheet">
+  </head>
+  <body>
+    <h3>Add Favourite</h3>
 <?php
-if((!isset($_POST['tag']) || $_POST['tag']=="")) {
-    //if not tags selected we set this variable to empty
-    $tags = "";
-} else if((isset($_POST['edittedtitle'])) && (isset($_POST['videoid'])) && (isset($_POST['tag']))) {
+
+if((isset($_POST['edittedtitle'])) && (isset($_POST['videoid']))) {
     $title = $_POST['edittedtitle'];
     $videoid = $_POST['videoid'];
-    $tags = $_POST['tag'];
-   //$name = $_POST['edittedtitle'];
     //First we add a new favourite
         try {
             //connection details for database held in config.ini file
@@ -26,17 +34,39 @@ if((!isset($_POST['tag']) || $_POST['tag']=="")) {
                 ":title" => $title,
                 ":videoid" => $videoid
             ));
-            $lastId = $db->lastInsertId();
-            if(isset($tags)){            
+            $lastFavouriteId = $db->lastInsertId();
+            //Now let's check for checkbox tags
+            if (isset($_POST['tag'])) {
+                //if no tags selected we set this variable to empty
+                $tags = $_POST['tag'];
                 $stmt = $db->prepare("insert into favourite_tags (favouriteid_FK, tagid_FK) values (:favourite_id, :tagid)");
                 foreach ($tags as $tag) {
                     $stmt->execute(array(
-                    ":favourite_id" => $lastId,
-                    ":tagid" => $tag)
+                        ":favourite_id" => $lastFavouriteId,
+                        ":tagid" => $tag)
                     );
                 }
-            }           
+            } 
+            //Now let's check for new tags
+            if (isset($_POST['newtags'])) {
+                //if not tags selected we set this variable to empty
+                $newtags = $_POST['newtags'];
+                $newtag = array_map('trim', explode(',', $newtags));
+                foreach ($newtag as $tag) {
+                    $stmt = $db->prepare("insert into tags (tag) values (:tag)");
+                    $stmt->bindParam(':tag', $tag, PDO::PARAM_INT);        
+                    $stmt->execute();
+                    $lastTagId = $db->lastInsertId();
+                    $stmt = $db->prepare("insert into favourite_tags (favouriteid_FK, tagid_FK) values (:favourite_id, :tagid)");
+                    $stmt->execute(array(
+                        ":favourite_id" => $lastFavouriteId,
+                        ":tagid" => $lastTagId)
+                    );
+                }  
+            } 
             $stmt->closeCursor();
+            printf('<p>Congratulations you have added "%s"</p>', $title);
+            printf("<a href=searchfavourites.php> Search Favourites</a>");
         } 
         catch (PDOException $e) {
             printf("We have a problem: %s\n ", $e->getMessage());
@@ -50,3 +80,5 @@ if((!isset($_POST['tag']) || $_POST['tag']=="")) {
     }
 
 ?>
+  </body>
+</html>
