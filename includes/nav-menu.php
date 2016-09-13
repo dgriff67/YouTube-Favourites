5f1d7f1783dd1e7db6ade8b751f65ec92ea58dff
David Griffiths <dgriff_67@yahoo.com>
2016-09-13 19:00:34 +0100

    navbar for inclusion in all pages

---
+++ includes/nav-menu.php
@@ -0,0 +1,25 @@
+<?php 
+$navbar = <<<END
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">YouTube Favourites</a>
+        </div>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <li><a href="index.php">YouTube</a></li>
+                <li><a href="searchfavourites.php">Favourites</a></li>
+                <li><a href="managetags.php">Tags</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+END;
+?>
