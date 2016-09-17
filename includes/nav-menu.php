5464c28758f65a1d7068bf662c5d3e67307c00b5
David Griffiths <dgriff_67@yahoo.com>
2016-09-17 16:36:26 +0100

    Brand label link now directs to index.php

--- includes/nav-menu.php
+++ includes/nav-menu.php
@@ -9,7 +9,7 @@ $navbar = <<<END
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#">YouTube Favourites</a>
+            <a class="navbar-brand" href="index.php">YouTube Favourites</a>
         </div>
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
